74a6c2bfc2c686e056542a99393b055f8d3b886f
hama1185 <hamataku0120@gmail.com>
2021-05-13 12:00:39 +0900

    バラクーダの導入およびテストコードの作成

---
+++ Barracuda/Assets/Scripts/TestModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Barracuda;
+//画風変換のテスト
+public class TestModel : MonoBehaviour
+{
+    public NNModel modelasset;
+    public RenderTexture input;
+    public RenderTexture output;
+
+    private Model runtimemodel;
+    private IWorker worker;
+
+    // Start is called before the first frame update
+    void Start(){
+        runtimemodel = ModelLoader.Load(modelasset);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Compute, runtimemodel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // 推論
+    void Inference(Tensor input){
+        worker.Execute(input);
+        Tensor outTensor = worker.PeekOutput();
+        outTensor.ToRenderTexture(output, 0, 0, 1/255f, 0, null);
+        outTensor.Dispose();
+    }
+
+    void OnDestroy(){
+        worker?.Dispose();
+    }
+}
